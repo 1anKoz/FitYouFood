d0daefcd5b994a46e7bac56a69a4323bae051612
1anKoz <jankozinski@wp.pl>
2024-10-26 23:06:55 +0200

    Allow for user to login

--- FitYouFood/Controllers/AccountControler.cs
+++ FitYouFood/Controllers/AccountControler.cs
@@ -4,13 +4,39 @@ using FitYouFood.Core.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitYouFood.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountControler(UserManager<User> userManager, ITokenService _tokenService) : ControllerBase
+    public class AccountControler(UserManager<User> _userManager, ITokenService _tokenService, SignInManager<User> singInManager) : ControllerBase
     {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
+
+            if (user == null) return Unauthorized("Invalid username!");
+
+            var result = await singInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized("Username not found or Password was incrorect");
+
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user)
+                }
+            );
+        }
+
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
@@ -27,11 +53,11 @@ namespace FitYouFood.API.Controllers
                     Email = registerDto.Email,
                 };
 
-                var createdUser = await userManager.CreateAsync(user, registerDto.Password);
+                var createdUser = await _userManager.CreateAsync(user, registerDto.Password);
 
                 if (createdUser.Succeeded)
                 {
-                    var roleResult = await userManager.AddToRoleAsync(user, "User");
+                    var roleResult = await _userManager.AddToRoleAsync(user, "User");
                     if (roleResult.Succeeded)
                     {
                         return Ok(
