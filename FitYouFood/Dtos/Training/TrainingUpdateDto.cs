d02cfdea5752b6bf0c3afe1f6cd94580f695b4ea
1anKoz <jankozinski@wp.pl>
2024-10-29 22:53:45 +0100

    fix some conflicts

--- FitYouFood/Dtos/Training/TrainingUpdateDto.cs
+++ FitYouFood/Dtos/Training/TrainingUpdateDto.cs
@@ -1,6 +1,13 @@
-﻿namespace FitYouFood.API.Dtos.Training
+using FitYouFood.Core.Entities;
+
+namespace FitYouFood.API.Dtos.Training
 {
     public class TrainingUpdateDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<int> Difficulty { get; set; }
+        public ICollection<DateTime> WhenTrained { get; set; }
+        public ICollection<ExerciseData> ExerciseDatas { get; set; }
     }
 }
