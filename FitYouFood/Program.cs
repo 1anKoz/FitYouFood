d015f17b69d1f12e5ec27e17d5c004f2c21e7dae
1anKoz <jankozinski@wp.pl>
2024-11-12 14:15:00 +0100

    Dockerize app, able to view swagger via docker

--- FitYouFood/Program.cs
+++ FitYouFood/Program.cs
@@ -49,12 +49,11 @@ builder.Services.AddAuthentication(options =>
         ValidAudience = builder.Configuration["JWT:Audience"],
         ValidateIssuerSigningKey = true,
         IssuerSigningKey = new SymmetricSecurityKey(
-            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"]))
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])),
     };
 });
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -79,8 +78,8 @@ builder.Services.AddSwaggerGen(option =>
             {
                 Reference = new OpenApiReference
                 {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
                 }
             },
             new string[]{}
@@ -104,4 +103,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+// This ensures the app listens on 0.0.0.0:80 inside the container
+app.Run("http://0.0.0.0:80");
